50e759b13a81654bed9c91a845311a337ba6f99c
Moelmi89 <moelmi89@gmail.com>
2023-03-11 20:33:20 +0000

    comments model and index js updated

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const User = require('./user');
+const Comments = require('./comments');
+const Post = require('./post');
+
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comments, {
+    foreignKey: 'post_id'
+});
+
+Comments.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comments.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comments };
