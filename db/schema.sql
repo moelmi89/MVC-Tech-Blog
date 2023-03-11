4f1503034771212cafcd963548c5128a8a4a9815
Moelmi89 <moelmi89@gmail.com>
2023-03-11 22:36:10 +0000

    auth/helpers added, routes fild structure added

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,3 @@
--- Creating database to hold user details 
 DROP DATABASE IF EXISTS blog_db;
 
 CREATE DATABASE blog_db;
---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+module.exports = {
+    format_date: (date) => { 
+      const newDate = new Date(date)
+      // Format date as MM/DD/YYYY
+      return newDate.toLocaleDateString();
+    },
+  };
